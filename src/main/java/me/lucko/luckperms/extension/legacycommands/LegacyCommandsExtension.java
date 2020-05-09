4ba3e10e2bbb06c24a3606d34a18dda0b805088e
Luck <git@lucko.me>
2020-05-09 19:35:06 +0100

    Replace the CommandExecutor instead of listening to events

--- src/main/java/me/lucko/luckperms/extension/legacycommands/LegacyCommandsExtension.java
+++ src/main/java/me/lucko/luckperms/extension/legacycommands/LegacyCommandsExtension.java
@@ -1,62 +1,38 @@
 package me.lucko.luckperms.extension.legacycommands;
 
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.command.PluginCommand;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerCommandPreprocessEvent;
-import org.bukkit.event.server.ServerCommandEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Consumer;
+import java.util.Objects;
 
-public class LegacyCommandsExtension extends JavaPlugin implements Listener {
+public class LegacyCommandsExtension extends JavaPlugin implements CommandExecutor {
+    private PluginCommand luckPermsCommand;
+    private CommandExecutor executor;
 
     @Override
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(this, this);
+        PluginCommand luckPermsCommand = getServer().getPluginCommand("luckperms");
+        Objects.requireNonNull(luckPermsCommand, "luckPermsCommand");
+        this.executor = luckPermsCommand.getExecutor();
+        luckPermsCommand.setExecutor(this);
     }
 
-    @EventHandler
-    public void onPlayerCommand(PlayerCommandPreprocessEvent e) {
-        onCommand(e.getMessage(), e::setMessage);
+    @Override
+    public void onDisable() {
+        this.luckPermsCommand.setExecutor(this.executor);
     }
 
-    @EventHandler
-    public void onServerCommand(ServerCommandEvent e) {
-        onCommand(e.getCommand(), e::setCommand);
-    }
-
-    private void onCommand(String buffer, Consumer<String> modification) {
-        if (buffer.isEmpty()) {
-            return;
-        }
-
-        String prefix = "";
-
-        if (buffer.charAt(0) == '/') {
-            buffer = buffer.substring(1);
-            prefix = "/";
-        }
-
-        int firstSpace = buffer.indexOf(' ');
-        if (firstSpace < 0) {
-            return;
-        }
-
-        String commandLabel = buffer.substring(0, firstSpace);
-        Command command = CommandMapUtil.getCommandMap(getServer()).getCommand(commandLabel);
-
-        if (command instanceof PluginCommand && ((PluginCommand) command).getPlugin().getName().equals("LuckPerms")) {
-            String argsString = buffer.substring(firstSpace + 1);
-            List<String> arguments = new ArrayList<>(Arrays.asList(argsString.split(" ")));
-
-            handleRewrites(arguments, true);
-            modification.accept(prefix + commandLabel + " " + String.join(" ", arguments));
-        }
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        List<String> arguments = new ArrayList<>(Arrays.asList(args));
+        handleRewrites(arguments, true);
+        return this.executor.onCommand(sender, command, label, arguments.toArray(new String[0]));
     }
 
     /* https://github.com/lucko/LuckPerms/blob/5577e3f59068a10d80201a6e07e60f620e5c654f/common/src/main/java/me/lucko/luckperms/common/command/CommandManager.java#L335 */
